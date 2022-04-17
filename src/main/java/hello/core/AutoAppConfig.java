d4e5a663cd792034315d3d65938c41ccd5583206
jin-lee <pplaneoo@gmail.com>
2022-04-17 17:12:19 +0900

    Component Scan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -15,4 +15,12 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    // 수동으로 등록한 빈과 자동으로 등록된 빈의 이름이 같아서 충돌
+    // 스프링에서는 수동으로 등록된 빈이 우선되어서 오버라이딩
+    // 부트에서는 디폴트로 오류를 발생하도록 바뀜(application.properties 세팅으로 바꿀 수 있음)
+    // 애초에 중복 등록 되지 않도록 구성하는 것이 좋음
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
